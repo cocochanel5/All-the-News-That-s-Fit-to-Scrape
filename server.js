9fa10344abfb0101c4f4d2e678e87110bac6a3fe
Chanel Farrell <chanel.farrell3@gmail.com>
2019-10-19 11:32:58 -0700

    should be working, but no images

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser"); 
+const mongoose = require("mongoose"); 
+const request = require("request"); 
+const cheerio = require("cheerio"); 
+
+
+const db = require("./models");
+
+const PORT = process.env.PORT || process.argv[2] || 8080;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+const router = require("./controllers/api.js");
+app.use(router);
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function () {
+    console.log(`This application is running on port: ${PORT}`);
+});
